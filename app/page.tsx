ef4bb32ce4be628258eb989c292f692313f6f194
Yichen Huang <yichen41@illinois.edu>
2024-06-15 17:52:09 -0500

    Added Approach Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
@@ -15,6 +16,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
